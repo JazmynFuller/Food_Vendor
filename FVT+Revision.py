10d042ae9da835011e88b2bbe87ff8da4745208b
Jazmyn <Jazmyn.fuller33@qmail.cuny.edu>
2018-02-27 00:27:08 -0500

    Trying to fix map

--- FVT+Revision.py
+++ FVT+Revision.py
@@ -5,15 +5,16 @@
 # 
 # { name : Enter the name of desired Food Vendor}
 
-# In[35]:
+# In[117]:
 
 
 #CrossCompute
 name = ''
 target_folder = '/tmp'
+target_folder_map = '/mapTmp'
 
 
-# In[36]:
+# In[118]:
 
 
 import pandas as pd
@@ -21,14 +22,14 @@ url = 'https://www.nycgovparks.org/bigapps/DPR_Eateries_001.json'
 fvt = pd.read_json(url, convert_dates=['state_date', 'end_date'])
 
 
-# In[37]:
+# In[119]:
 
 
 selected_fvt = fvt[fvt['name'].str.contains(name, case=False)]
-#selected_fvt[:249]
+selected_fvt[:249]
 
 
-# In[38]:
+# In[120]:
 
 
 length = len(selected_fvt)
@@ -40,13 +41,13 @@ elif(length>1):
     print("There are %s" %length + " matching Food Vendors with this name")
 
 
-# In[39]:
+# In[121]:
 
 
 print("Vendors_with_matching_name = %s" %length)
 
 
-# In[40]:
+# In[122]:
 
 
 #gives table of the expired vendor you searched.
@@ -55,22 +56,17 @@ from datetime import datetime
 now = datetime.now()
 filtered_fvt = selected_fvt[now > selected_fvt['end_date']]
 exp_len = len(filtered_fvt)
-if (exp_len>1):
-    print("There are %s" %exp_len + " expired Food Vendor Permits.")
-elif(exp_len==1):
-    print("There is 1 expired Food Vendor Permit with this name.")
-elif(exp_len==0):
-    print("There are no expired Food Vendor Permits with this name.")
-filtered_fvt[['name','location','park_id', 'end_date'][:249]]
+print("There are %s" %exp_len + " expired Food Vendor Permits.")
+filtered_fvt[:249]
 
 
-# In[41]:
+# In[123]:
 
 
 print("Vendors_with_expired_license = %s" % exp_len)
 
 
-# In[44]:
+# In[124]:
 
 
 target_path = target_folder + '/table.csv'
@@ -78,7 +74,19 @@ filtered_fvt.to_csv(target_path, index=False)
 print('Expired_Vendor_table_path = %s' % target_path)
 
 
+# In[125]:
+
+
+#drop null values in specified columns
+target_folder_map = '/tmp'
+map_fvt = fvt.dropna(subset=['location'])
+target_path = target_folder_map + '/map.csv'
+filtered_fvt.to_csv(target_path, index=False)
+print('vendor_geotable_path = %s' % target_path)
+
+
 # # Vendor Status
 # 
-# {Vendors_with_matching_name : Total number of Food Vendors with this name: }
-# {Vendors_with_expired_license : Food Vendors with this name and expired licenses: }
+# #{Vendors_with_matching_name : Total number of Food Vendors with this name: }
+# #{Vendors_with_expired_license : Food Vendors with this name and expired licenses: }
+# 
